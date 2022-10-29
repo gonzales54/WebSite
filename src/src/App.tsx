e111897488bcd2490adac1536908158b4ba281e2
gonzales54 <gonzalesitokawa@gmail.com>
2022-10-29 14:14:32 +0900

    start

---
+++ src/src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+
+  return (
+    <div className="App">
+      <header>
+        <h1>Hello</h1>
+      </header>
+      <main>1</main>
+      <footer>1</footer>
+    </div>
+  )
+}
+
+export default App
