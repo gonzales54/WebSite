8ac200fc6b7cf9b1507d12e2de13c142297c5d12
gonzales54 <gonzalesitokawa@gmail.com>
2022-11-01 16:33:20 +0900

    add header

--- src/src/App.tsx
+++ src/src/App.tsx
@@ -1,12 +1,35 @@
-import HeaderComponent from "./components/HeaderComponent"
-import MainComponent from "./components/MainComponent"
-import FooterComponent from "./components/FooterComponent"
+import HeaderComponent from "./components/HeaderComponent";
+import MainComponent from "./components/MainComponent";
+import FooterComponent from "./components/FooterComponent";
+import WebSiteImageURL from "./assets/website.jpg";
+import './assets/reset.css';
+import './scss/utility.scss';
+import './scss/atom.scss';
+import './scss/molecules.scss';
+import './scss/organisms.scss';
+import { useState } from "react";
 
 function App() {
+  const [isNavOpen, setNavOpen] = useState<boolean>(false);
+  const navOpen = () => {
+    setNavOpen(!isNavOpen)
+    if (isNavOpen) {
+      document.body.style.overflow = "auto";
+    } else {
+      document.body.style.overflow = "hidden";
+    }
+  }
+  const work = [
+    {
+      title: 'react-website',
+      image: WebSiteImageURL,
+      url: '/'
+    }
+  ]
   return (
     <div className="App">
-      <HeaderComponent/>
-      <MainComponent/>
+      <HeaderComponent isNavOpen={isNavOpen} navOpen={navOpen} />
+      <MainComponent />
       <FooterComponent/>
     </div>
   )
--- src/src/components/HeaderComponent.tsx
+++ src/src/components/HeaderComponent.tsx
@@ -1,7 +1,36 @@
-const HeaderComponent = () => {
+const HeaderComponent = ({ isNavOpen, navOpen }: any) => {
 	return (
-		<header>
-			<h1>Header</h1>
+		<header className='header'>
+			<div className='container-lg flex justify-between align-items-center'>
+				<h1>
+					<a href="" className='block logo'>Itokawa</a>
+				</h1>
+				<button className='nav-btn sp-only' onClick={navOpen}>
+					{isNavOpen
+						?
+						<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="nav-icon">
+							<path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+						</svg>
+						:
+						<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className='nav-icon'>
+							<path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+						</svg>
+					}
+				</button>
+				<nav className={isNavOpen ? 'nav nav-open' : 'nav'}>
+					<ul className='flex'>
+						<li className='nav-item'>
+							<a href="" className='block nav-link'>Home</a>
+						</li>
+						<li className='nav-item'>
+							<a href="" className='block nav-link'>About</a>
+						</li>
+						<li className='nav-item'>
+							<a href="" className='block nav-link'>Work</a>
+						</li>
+					</ul>
+				</nav>
+			</div>
 		</header>
 	)
 }
