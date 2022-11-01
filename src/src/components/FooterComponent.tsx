52226f164cd78603f4910c7682f655054a033817
gonzales54 <gonzalesitokawa@gmail.com>
2022-11-01 16:43:00 +0900

    add footer

--- src/src/components/FooterComponent.tsx
+++ src/src/components/FooterComponent.tsx
@@ -1,9 +1,41 @@
-const FooterComponent = () => {
+import TwitterIcon from '../assets/twitter.png'
+
+const Footer = () => {
 	return (
-		<footer>
-			<h1>Footer</h1>
+		<footer className='footer'>
+			<div className="container-md">
+				<div className='footer-content'>
+					<h3 className='footer-logo'>
+						<a href="">Itokawa</a>
+					</h3>
+					<ul className='footer-container flex justify-center'>
+						<li className='footer-item'>
+							<a href="" className='footer-link block'>Home</a>
+						</li>
+						<li className='footer-item'>
+							<a href="" className='footer-link block'>About</a>
+						</li>
+						<li className='footer-item'>
+							<a href="" className='footer-link block'>Work</a>
+						</li>
+					</ul>
+					<div className='social'>
+						<h4 className='social-title'>SNS</h4>
+						<div className='social-container flex justify-center'>
+							<a href="" className='social-link block'>
+								<p className='social-icon '>
+									<img src={TwitterIcon} alt="" className='block' />
+								</p>
+							</a>
+						</div>
+					</div>
+				</div>
+				<p className='small'>
+					<small>&copy; Made By Itokawa</small>
+				</p>
+			</div>
 		</footer>
 	)
 }
 
-export default FooterComponent
+export default Footer
